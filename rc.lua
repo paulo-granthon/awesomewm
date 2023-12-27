ebeff2cc18b99bb1520c4d6a939939a278a4bbba
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-12-27 02:48:41 -0300

    feat: add default application icon

--- rc.lua
+++ rc.lua
@@ -632,6 +632,19 @@ client.connect_signal("manage", function(c)
     end
 end)
 
+client.connect_signal("manage", function(c)
+    local cairo = require("lgi").cairo
+    local default_icon = "/home/" .. os.getenv("USER") .. "/.config/awesome/icons/arch256.png"
+    if c and c.valid and not c.icon then
+        local s = gears.surface(default_icon)
+        local img = cairo.ImageSurface.create(cairo.Format.ARGB32, s:get_width(), s:get_height())
+        local cr = cairo.Context(img)
+        cr:set_source_surface(s, 0, 0)
+        cr:paint()
+        c.icon = img._native
+    end
+end)
+
 -- Add a titlebar if titlebars_enabled is set to true in the rules.
 client.connect_signal("request::titlebars", function(c)
     -- buttons for the titlebar
