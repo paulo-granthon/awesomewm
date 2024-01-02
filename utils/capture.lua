b73b2646193d4e33adc12a6d0a3ff3328bddcdb1
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-01-02 19:21:32 -0300

    feat: `os.execute` - run and capture results of `bash` scripts in `utils.capture`

---
+++ utils/capture.lua
@@ -0,0 +1,21 @@
+function os.capture(command)
+    local tmpfile = '/tmp/lua_execute_tmp_file'
+    local exit = os.execute(command .. ' > ' .. tmpfile .. ' 2> ' .. tmpfile .. '.err')
+
+    local stdout_file = io.open(tmpfile)
+    if not stdout_file then
+        return nil, nil, "os.capture: stdout_file Error"
+    end
+    local stdout = stdout_file:read("*all")
+
+    local stderr_file = io.open(tmpfile .. '.err')
+    if not stderr_file then
+        return nil, nil, "os.capture: stderr_file Error"
+    end
+    local stderr = stderr_file:read("*all")
+
+    stdout_file:close()
+    stderr_file:close()
+
+    return exit, stdout, stderr
+end
