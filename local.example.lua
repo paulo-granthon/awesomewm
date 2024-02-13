e2816e005d95ad8b9a6e4f07213a36fe87054ff3
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-13 11:42:05 -0300

    chore: rename `local` to `local.example`

---
+++ local.example.lua
@@ -0,0 +1,6 @@
+local M = {}
+
+M.use_brightness_widget = true
+M.use_batteryarc_widget = true
+
+return M
--- local.lua
+++
@@ -1,6 +0,0 @@
-local M = {}
-
-M.use_brightness_widget = false
-M.use_batteryarc_widget = false
-
-return M
