a58ea5518bfe316d4c4df727aa558cc29068a58f
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-13 11:33:23 -0300

    refactor: return function instead of module in `utils.capture`

--- utils/capture.lua
+++ utils/capture.lua
@@ -1,25 +1,19 @@
-local M = {}
+local os_capture = function(command)
+  local tmpfile = '/tmp/lua_execute_tmp_file'
+  local exit = os.execute(command .. ' > ' .. tmpfile .. ' 2> ' .. tmpfile .. '.err')
 
-function M.os_capture(command)
-    local tmpfile = '/tmp/lua_execute_tmp_file'
-    local exit = os.execute(command .. ' > ' .. tmpfile .. ' 2> ' .. tmpfile .. '.err')
+  local stdout_file = io.open(tmpfile)
+  if not stdout_file then return nil, nil, 'os.capture: stdout_file Error' end
+  local stdout = stdout_file:read('*all')
 
-    local stdout_file = io.open(tmpfile)
-    if not stdout_file then
-        return nil, nil, "os.capture: stdout_file Error"
-    end
-    local stdout = stdout_file:read("*all")
+  local stderr_file = io.open(tmpfile .. '.err')
+  if not stderr_file then return nil, nil, 'os.capture: stderr_file Error' end
+  local stderr = stderr_file:read('*all')
 
-    local stderr_file = io.open(tmpfile .. '.err')
-    if not stderr_file then
-        return nil, nil, "os.capture: stderr_file Error"
-    end
-    local stderr = stderr_file:read("*all")
+  stdout_file:close()
+  stderr_file:close()
 
-    stdout_file:close()
-    stderr_file:close()
-
-    return exit, stdout, stderr
+  return exit, stdout, stderr
 end
 
-return M
+return os_capture
