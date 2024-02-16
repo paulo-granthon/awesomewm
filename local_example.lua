fd6be43e2a768514eb569d35d6e705bac7b77623
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-16 02:14:11 -0300

    refactor: default `local_example` variables to `is_portable`

--- local_example.lua
+++ local_example.lua
@@ -1,6 +1,8 @@
 local M = {}
 
-M.use_brightness_widget = true
-M.use_batteryarc_widget = true
+local is_portable = true
+
+M.use_brightness_widget = is_portable
+M.use_batteryarc_widget = is_portable
 
 return M
